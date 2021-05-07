ff45e1dd1a5bb159c6087da9eec02c7ce853b5fa
kukimik <kukimik@poczta.onet.pl>
2021-05-07 23:02:35 +0200

    work on sql writer (#6)

--- src/Morela/Writer/SQL.hs
+++ src/Morela/Writer/SQL.hs
@@ -3,7 +3,7 @@
 module Morela.Writer.SQL (diagramToSQL) where
 
 import Control.Arrow ((&&&))
-import Data.Maybe (fromMaybe)
+import Data.List (intersperse)
 import qualified Data.Set as Set
 import qualified Data.Text.Lazy as L
 import Data.Text.Lazy.Builder
@@ -11,34 +11,51 @@ import qualified Morela.Types as MR
 
 diagramToSQL :: MR.Diagram -> L.Text
 diagramToSQL =
-  toText .
-  mconcat .
-  (uncurry (<>)) .
-  unzip .
-  fmap (
-        bldCreateTable
-        &&&
-        bldAddForeignKeys
-       ) .
-  diagramTables
+  toLazyText
+    . mconcat
+    . fmap
+      ( uncurry (<>)
+          . ( bldCreateTable
+                &&& bldAddForeignKeys
+            )
+      )
+    . Set.toList
+    . MR.diagramTables
 
 bldCreateTable :: MR.Table -> Builder
-bldCreateTable = undefined
+bldCreateTable MR.Table {MR.tableName = tn, MR.tableAttributes = as, MR.tablePK = pk, MR.tableNNs = nns, MR.tableCKs = cks, MR.tableUQs = uqs} =
+  mconcat
+    [ textsToBld ["CREATE TABLE ", tn, "(\n"],
+      bldAttrs,
+      bldPK,
+      bldUQs,
+      bldCKs,
+      fromLazyText ");\n"
+    ]
+  where
+    bldAttrs = undefined
+    bldPK = undefined
+    bldCKs = undefined
+    bldUQs = undefined
 
 bldAddForeignKeys :: MR.Table -> Builder
-bldAddForeignKeys _{tableFKs=fks,tableName=tn,...} =
+bldAddForeignKeys MR.Table {MR.tableFKs = fks, MR.tableName = tn} =
   mconcat $ bldAddForeignKey tn <$> fks
 
 bldAddForeignKey :: MR.TableName -> MR.FKConstraint -> Builder
-bldAddForeignKey tn fk = mconcat . fmap fromText $
-  ["ALTER TABLE "
-  ,fromText tn
-  ," ADD FOREIGN KEY("
-  ,L.intercalate "," c1s -- builder!!!
-  ,") REFERENCES "
-  ,fkReferencedTableName fk
-  ,"("
-  ,L.intercalate "," c2s -- builder!!!
-  ,");\n"
-  ]
-  where (c1s,c2s) = fkAttributeMapping fk
+bldAddForeignKey tn fk =
+  mconcat
+    [ textsToBld ["ALTER TABLE ", tn, " ADD FOREIGN KEY("],
+      bldCSV c1s, -- builder!!!
+      textsToBld [") REFERENCES ", MR.fkReferencedTableName fk, "("],
+      bldCSV c2s, -- builder!!!
+      fromLazyText ");\n"
+    ]
+  where
+    (c1s, c2s) = unzip $ MR.fkAttributeMapping fk
+
+textsToBld :: [L.Text] -> Builder
+textsToBld = mconcat . fmap fromLazyText
+
+bldCSV :: [L.Text] -> Builder
+bldCSV = mconcat . fmap fromLazyText . intersperse ","
